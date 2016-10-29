1a56a57c1f3216cb9fbdd5bc485aaaa3a806904b
akawashiro <a.kawashiro@gmail.com>
2016-10-29 21:20:58 +0900

    First commit.

---
+++ D_mail.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*- 
+
+import smtplib
+from email.MIMEText import MIMEText
+from email.Header import Header
+from email.Utils import formatdate
+
+from_address = 'a.kawashiro@gmail.com'
+to_address   = 'a.kawashiro@gmail.com'
+
+charset = 'ISO-2022-JP'
+subject = u'メールの件名です'
+text    = u'メールの本文です'
+
+msg = MIMEText(text.encode(charset), 'plain', charset)
+msg['Subject'] = Header(subject, charset)
+msg['From'] = from_address
+msg['To'] = to_address
+msg['Date'] = formatdate(localtime=True)
+
+smtp = smtplib.SMTP()
+smtp.connect()
+smtp.sendmail(from_address, to_address, msg.as_string())
+smtp.close()
+
---
+++ README.md
@@ -0,0 +1,4 @@
+# D-mail
+This repository is made to send e-mail to the past.
+
+# Let's start Operation Skuld!
