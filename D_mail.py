79f3bb8625a9cc766480e63e54673b18b5b01665
Akira KAWATA <a.kawashiro@gmail.com>
2016-11-01 16:42:34 +0900

    Now I can send D-mail from the future.

--- D_mail.py
+++ D_mail.py
@@ -1,25 +1,47 @@
-# -*- coding: utf-8 -*- 
+#!/Usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import smtplib
-from email.MIMEText import MIMEText
-from email.Header import Header
+from email.mime.text import MIMEText
 from email.Utils import formatdate
+import time
+from datetime import datetime as dt
+import argparse
 
-from_address = 'a.kawashiro@gmail.com'
-to_address   = 'a.kawashiro@gmail.com'
 
-charset = 'ISO-2022-JP'
-subject = u'メールの件名です'
-text    = u'メールの本文です'
+if __name__ == '__main__':
+    psr = argparse.ArgumentParser()
+    psr.add_argument('--user', required=True)
+    psr.add_argument('--password', required=True)
+    psr.add_argument('--to', default="None")
+    psr.add_argument('--hour', required=True)
+    psr.add_argument('--body', required=True)
+    args = psr.parse_args()
 
-msg = MIMEText(text.encode(charset), 'plain', charset)
-msg['Subject'] = Header(subject, charset)
-msg['From'] = from_address
-msg['To'] = to_address
-msg['Date'] = formatdate(localtime=True)
+    username, password = args.user, args.password
 
-smtp = smtplib.SMTP()
-smtp.connect()
-smtp.sendmail(from_address, to_address, msg.as_string())
-smtp.close()
+    if args.to == "None":
+        to = args.user
+    else:
+        to = args.to
 
+    sub = ''
+    body = args.body
+    ubody = unicode(body, 'utf-8')
+
+    for b in [ubody[i:i + 5] for i in range(0, len(ubody), 5)]:
+        host, port = 'smtp.gmail.com', 465
+        msg = MIMEText(b.encode('utf-8'))
+        msg['Subject'] = sub
+        msg['From'] = username
+        msg['To'] = to
+        n = dt.now()
+        msg['Date'] = formatdate(time.mktime((n.year, n.month,
+                                              n.day, n.hour + int(args.hour), n.minute, n.second, n.weekday(), 0, 0)))
+        smtp = smtplib.SMTP_SSL(host, port)
+        smtp.ehlo()
+        smtp.login(username, password)
+        smtp.mail(username)
+        smtp.rcpt(to)
+        smtp.data(msg.as_string())
+        smtp.quit()
